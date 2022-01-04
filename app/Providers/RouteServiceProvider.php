0386058148773c91693ee46eb5eedf7c42ce8f98
DevaJayantha <devajayantha870@gmail.com>
2022-01-04 15:27:01 +0800

    add plugin timedoor membership

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,6 +46,17 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+            Route::prefix('api')
+                ->middleware('api')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/membership.php'));
+
+            Route::middleware('web')
+                ->prefix('admin')
+                ->as('admin.')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/admin.php'));
         });
     }
 
